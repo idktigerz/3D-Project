86e171ba3859667e884dd53f372a370610c23ff9
idktigerz <idktigerzz@gmail.com>
2023-03-22 15:43:08 +0000

    a

--- 3D Project/Assets/Scipts/TimeController.cs
+++ 3D Project/Assets/Scipts/TimeController.cs
@@ -45,7 +45,8 @@ public class TimeController : MonoBehaviour
 
     [SerializeField]
     private float maxMoonLightIntensity;
-    
+
+    [Header("Skybox texture")]
     [SerializeField]
     private Material nightSky;
     [SerializeField]
