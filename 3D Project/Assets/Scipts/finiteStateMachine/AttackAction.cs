97a243933cd8c4ddf06cbeed7274a1b8082c3bbd
AkaKoichi <bruninhomartins240@gmail.com>
2023-04-13 00:00:29 +0100

    a

--- 3D Project/Assets/Scipts/finiteStateMachine/AttackAction.cs
+++ 3D Project/Assets/Scipts/finiteStateMachine/AttackAction.cs
@@ -7,7 +7,6 @@ public class AttackAction : Action
 {
     public override void Act(FiniteStateMachine fsm)
     {
-        Debug.Log($"dddddddddddddddddddddddddddd");
         if (fsm.gameObject.name == "Crocodile") fsm.GetNavMeshAgent().CrocodileAttack();
         else if (fsm.gameObject.name == "Snake") fsm.GetNavMeshAgent().SnakeAttack();
 
