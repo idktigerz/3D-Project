2c97f8e5c21c36c63b655b76e8a1f634c8d9ef05
idktigerz <idktigerzz@gmail.com>
2023-04-16 02:44:28 +0100

    a

--- 3D Project/Assets/Scripts/GameManager.cs
+++ 3D Project/Assets/Scripts/GameManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject pauseMenu;
+    public PlayerController playerController;
+    public TimeController timeController;
+
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +26,10 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         ProcessInputs();
+        if(timeController.dayCounter == 7)
+        {
+            Endgame();
+        }
     }
 
     private void ProcessInputs()
@@ -61,5 +71,20 @@ public class GameManager : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void Endgame()
+    {
+        Time.timeScale = 0;
+        if(playerController.points < 10000)
+        {
+            scoreText.text = "You're fired!\n You scored: " + playerController.points;
+        }else if(playerController.points > 10000 && playerController.points < 20000)
+        {
+            scoreText.text = "You did okay\n You scored: " + playerController.points;
+        }else if(playerController.points > 20000)
+        {
+            scoreText.text = "Good job!\n You scored: " + playerController.points;
+        }
     }
 }
