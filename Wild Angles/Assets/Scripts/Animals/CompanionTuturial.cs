a82c7bd6d768d00dfdb8439de24f882419b3a7ba
AkaKoichi <bruninhomartins240@gmail.com>
2023-05-18 10:38:34 +0100

    a

---
+++ Wild Angles/Assets/Scripts/Animals/CompanionTuturial.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompanionTuturial : MonoBehaviour
+{
+    [SerializeField] bool canStartMove;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canStartMove = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canStartMove)
+        {
+            
+        }
+    }
+}
