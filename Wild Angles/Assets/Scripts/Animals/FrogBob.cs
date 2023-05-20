5ccd1907026bcc9e1cbad21132211732ad875764
AkaKoichi <bruninhomartins240@gmail.com>
2023-05-20 04:00:08 +0100

    A

--- Wild Angles/Assets/Scripts/Animals/FrogBob.cs
+++ Wild Angles/Assets/Scripts/Animals/FrogBob.cs
@@ -12,17 +12,20 @@ public class FrogBob : MonoBehaviour
     private float timer;
 
     private Vector3 moveDirection;
+    private bool canBob;
 
     // Start is called before the first frame update
     void Start()
     {
+        canBob = false;
         defaultYPos = 1.104f;
+        StartCoroutine("FlyBobHandlertime");
     }
 
     // Update is called once per frame
     void Update()
     {
-        FlyBobHandler();
+        if (canBob) FlyBobHandler();
     }
     private void FlyBobHandler()
     {
@@ -34,5 +37,10 @@ public class FrogBob : MonoBehaviour
                 transform.localPosition.z);
         }
 
+    }
+    private IEnumerator FlyBobHandlertime()
+    {
+        yield return new WaitForSeconds(Random.Range(1, 7));
+        canBob = true;
     }
 }
